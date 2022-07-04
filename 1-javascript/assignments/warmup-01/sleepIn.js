0cb939b94f7d2063d1556089ad735ddcc36f89b5
kaushal1610 <you@example.com>
2022-07-04 09:25:26 +0530

    solved_sleepin

---
+++ 1-javascript/assignments/warmup-01/sleepIn.js
@@ -0,0 +1,12 @@
+function sleepIn(weekday, vacation){
+    if (weekday == true && vacation == false)
+  
+      return false;
+  
+    else
+  
+      return true;
+  }
+  
+let result =   sleepIn(true , true);
+console.log(result);
