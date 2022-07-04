27df3895c334fabdae33f29ffea38a815c4d6364
kaushal1610 <you@example.com>
2022-07-04 17:44:40 +0530

    solved_loneteen

---
+++ 1-javascript/assignments/warmup-01/loneteen.js
@@ -0,0 +1,9 @@
+function loneTeen(a, b){
+    if((a > 12 & a < 20) ^ (b > 12 & b < 20))
+      return true;
+    else 
+      return false;
+    
+  }
+  let result =   loneTeen(13,13);
+console.log(result);
