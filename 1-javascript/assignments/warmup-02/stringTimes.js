d7b76feca0d201867108e016ca7da87fe1dd23b4
kaushal1610 <you@example.com>
2022-07-04 22:42:05 +0530

    solved_stringTimes

---
+++ 1-javascript/assignments/warmup-02/stringTimes.js
@@ -0,0 +1,9 @@
+function stringTimes(str, n){
+    let result = "";
+    for (let i=0; i<n; i++) {
+     result += str;  
+   }
+   return result;
+ }
+ let result =   stringTimes('hi',2);
+ console.log(result);
