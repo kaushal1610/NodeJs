5e0729e37650ca7cee22d2bc300d99e198b91020
kaushal1610 <you@example.com>
2022-07-05 10:24:23 +0530

    solved_endup

---
+++ 1-javascript/assignments/warmup-01/endUp.js
@@ -0,0 +1,9 @@
+function endUp(str){
+  if(str.length<=3)
+    return str.toUpperCase();
+  else
+   return str.substring(0,str.length-3) + str.substring(str.length-3).toUpperCase();
+}
+
+let result =   frontTimes('hi');
+console.log(result); 
