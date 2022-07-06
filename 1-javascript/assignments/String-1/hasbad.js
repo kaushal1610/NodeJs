e0dbca95ca4d8b39ada949dbd1b393bb89299cce
kaushal1610 <you@example.com>
2022-07-06 21:38:28 +0530

    solved_both

---
+++ 1-javascript/assignments/String-1/hasbad.js
@@ -0,0 +1,8 @@
+function hasBad(str){
+    if(str.substring(0,3)=="bad"||str.substring(1,4)=="bad")
+      return true
+    else
+      return false
+  }
+  let result =  hasBad('badxx');
+  console.log(result);
