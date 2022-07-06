4f71be4a15428899190dc681592ddfe41d2cf5c1
kaushal1610 <you@example.com>
2022-07-06 16:59:44 +0530

    solved

---
+++ 1-javascript/assignments/String-1/middtwo.js
@@ -0,0 +1,5 @@
+function middleTwo(str,n){
+    return str.substring(str.length/ 2 - 1, str.length / 2 + 1);
+ }
+ let result =  middleTwo('string');
+ console.log(result);
--- 1-javascript/assignments/String-1/withoutEnd2.js
+++ 1-javascript/assignments/String-1/withoutEnd2.js
@@ -2,5 +2,5 @@ function withoutEnd2(str){
     let len = str.length;
     return str.slice(1,len - 1);
   }
-  let result =   withoutEnd2('ab');
+  let result =  withoutEnd2('ab');
   console.log(result);
