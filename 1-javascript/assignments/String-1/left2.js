3ce1f129d1c10b630ff5ba6b7193ce5170a33bca
kaushal1610 <you@example.com>
2022-07-06 12:40:18 +0530

    solved_update

---
+++ 1-javascript/assignments/String-1/left2.js
@@ -0,0 +1,5 @@
+function left2(str){
+    return str.substring(2, str.length) + str.substring(0,2);
+}
+let result =   left2('Hello');
+console.log(result);
--- 1-javascript/assignments/String-1/nonStart.js
+++ 1-javascript/assignments/String-1/nonStart.js
@@ -1,5 +1,5 @@
 function nonStart(a, b){ 
     return a.substring(1)+b.substring(1);
   }
-  let result =   nonStart('Java','code ');
+  let result =   nonStart('Java','code');
   console.log(result);
