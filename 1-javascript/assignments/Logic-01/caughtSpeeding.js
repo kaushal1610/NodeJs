632d932ae47afab03af9b4d2a54938e6be438f9a
kaushal1610 <you@example.com>
2022-07-07 23:24:27 +0530

    solved

---
+++ 1-javascript/assignments/Logic-01/caughtSpeeding.js
@@ -0,0 +1,17 @@
+function caughtSpeeding(speed, isBirthday){
+    if (!(isBirthday)) {
+      if (speed <= 60)
+        return 0;
+      if (speed > 60 && speed <= 80)
+        return 1;
+      else
+        return 2;
+    } else if (speed <=65)
+        return 0;
+      else if (speed > 65 && speed <= 85)
+        return 1;
+      else
+        return 2;
+  }
+  let result =  dateFashion(60,false);
+  console.log(result);
