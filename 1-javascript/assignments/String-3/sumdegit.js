99f0f900bf5821911f782df4b1623f3f27bdcdbd
kaushal1610 <you@example.com>
2022-07-07 21:59:22 +0530

    solved

---
+++ 1-javascript/assignments/String-3/sumdegit.js
@@ -0,0 +1,14 @@
+function sumDigits(str){
+    let sum = 0;
+       let lim = str.length;
+    let ch;
+    for(let i = 0; i < lim; i++)
+    {
+        ch = str.charAt(i);
+          if(ch >= '0' && ch <= '9') // or simply 
+              sum += (ch - '0');
+      }
+      return sum;
+  }
+  let result =   sumDigits('Chocolate');
+  console.log(result);
